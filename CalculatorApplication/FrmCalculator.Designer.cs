d56bb7be7da43093c73801851172d0b886256d0f
JanuaryToribioJr <toribiojanuaryjr@gmail.com>
2024-11-12 02:46:47 +0800

    Add project files.

---
+++ CalculatorApplication/FrmCalculator.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CalculatorApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.op = new System.Windows.Forms.ComboBox();
+            this.ans = new System.Windows.Forms.Label();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            this.num1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num1.Location = new System.Drawing.Point(226, 36);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(157, 26);
+            this.num1.TabIndex = 0;
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.num2.Location = new System.Drawing.Point(226, 115);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(157, 26);
+            this.num2.TabIndex = 1;
+            // 
+            // op
+            // 
+            this.op.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.op.FormattingEnabled = true;
+            this.op.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.op.Location = new System.Drawing.Point(247, 78);
+            this.op.Name = "op";
+            this.op.Size = new System.Drawing.Size(121, 26);
+            this.op.TabIndex = 2;
+            // 
+            // ans
+            // 
+            this.ans.AutoSize = true;
+            this.ans.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ans.Location = new System.Drawing.Point(244, 164);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(18, 18);
+            this.ans.TabIndex = 3;
+            this.ans.Text = " ";
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateBtn.Image = ((System.Drawing.Image)(resources.GetObject("calculateBtn.Image")));
+            this.calculateBtn.Location = new System.Drawing.Point(247, 199);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(110, 33);
+            this.calculateBtn.TabIndex = 4;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(142, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Answer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(399, 251);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.op);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.ComboBox op;
+        private System.Windows.Forms.Label ans;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CalculatorApplication/FrmCalculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class Form1 : Form
+    {
+        CalculatorClass calc = new CalculatorClass();
+        public delegate T Formula<T>(T n1, T n2);
+        public Form1(CalculatorClass calc)
+        {
+            this.calc = calc;
+            InitializeComponent();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class CalculatorClass 
+        {
+            public Formula<double> Total;
+            public event Formula<double> CalculateEvent
+            {
+                add
+                {
+                    MessageBox.Show("Added to delegate");
+                    Total += value;
+                }
+                remove
+                {
+                    MessageBox.Show("Removed to delegate");
+                    Total -= value;
+                }
+            }
+            public double GetSum(double num1, double num2)
+            {
+                return num1 + num2;
+            }
+            public double GetDifference(double num1, double num2)
+            {
+                return num1 - num2;
+            }
+            public double GetMultiplication(double num1, double num2)
+            {
+                return num1 * num2;
+            }
+            public double GetDivision(double num1, double num2)
+            {
+                return num1 / num2;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(num1.Text);
+            double n2 = Convert.ToDouble(num2.Text);
+            string Operator = op.SelectedItem.ToString();
+
+                if (Operator == "+")
+                {
+                    calc.CalculateEvent += new Formula<double>(calc.GetSum);
+                    ans.Text = calc.GetSum(n1, n2).ToString();
+                    calc.CalculateEvent -= new Formula<double>(calc.GetSum);
+                }
+                else if (Operator == "-")
+                {
+                    calc.CalculateEvent += new Formula<double>(calc.GetDifference);
+                    ans.Text = calc.GetDifference(n1, n2).ToString();
+                    calc.CalculateEvent -= new Formula<double>(calc.GetDifference);
+                }
+                else if (Operator == "*")
+                {
+                    calc.CalculateEvent += new Formula<double>(calc.GetMultiplication);
+                    ans.Text = calc.GetMultiplication(n1, n2).ToString();
+                    calc.CalculateEvent -= new Formula<double>(calc.GetMultiplication);
+                }
+                else if (Operator == "/")
+                {
+                    if (n2 != 0)
+                    {
+                        calc.CalculateEvent += new Formula<double>(calc.GetDivision);
+                        ans.Text = calc.GetDivision(n1, n2).ToString();
+                        calc.CalculateEvent -= new Formula<double>(calc.GetDivision);
+                    }
+                    else
+                    {
+                        ans.Text = "Undefined";
+                    }
+                }
+
+        }
+       
+    }
+
+}
